296f4a60d8a78a50a5407e5897e1ad26d50e936e
root <root@uday-WORKSTATION.com>
2020-12-24 01:05:47 +0530

    Changed the index 1 more time

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,18 +1,3 @@
 <?php
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {background-color: pink;}
-h1   {color: blue;}
-p    {color: blue;}
-</style>
-</head>
-<body>
-
-<h1>THIS IS SOWKHYA`s PAGE</h1>
-<p>SOWKHYA LIKES PINK.</p>
-
-</body>
-</html> 
+UDAY EDIT =========Hello, World! php version is =========XYZ7.3.11
 ?>
