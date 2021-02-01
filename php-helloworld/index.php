29cf961b36c9b3c8f284c6070cc6f788b36a579e
student <student@uday-WORKSTATION.com>
2021-02-01 23:29:34 +0530

    Inserted additional line on the index page

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! This is created via the console and php version is " . PHP_VERSION . "\n";
+print "Hello, World! This is created via the console and php version is " . PHP_VERSION . "\n";
 ?>
